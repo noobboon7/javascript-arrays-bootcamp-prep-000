807c6e700d29c178a49980fd825b451f8d9af045
Jerry Velazquez <jvr572@gmail.com>
2019-01-29 19:27:21 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,34 @@
+var chocolateBars = ["snickers",
+"hundred grand",
+"kitkat",
+"skittles"
+  ];
+function addElementToBeginningOfArray(array, element){
+  return array.unshift();
+}
+  
+function destructivelyAddElementToBeginningOfArray(array, element){
+  return array.unshift(element);
+}
+
+function addElementToEndOfArray(array, element){
+  return array.push;
+}
+function destructivelyAddElementToEndOfArray(array,element) {
+ return addElementToEndOfArray.push; 
+}
+function accessElementInArray(array, index){
+  return index;
+}
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return array.shift();
+}
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1);
+}
+function destructivelyRemoveElementFromEndOfArray(array){
+  return array.pop();
+}
+function removeElementFromEndOfArray(array){
+  return array.slice(0, array.length -1);
+}
