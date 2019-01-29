b4aa9b8e053f408cdbe0b1bb66284e57ca94939c
Jerry Velazquez <jvr572@gmail.com>
2019-01-29 20:51:43 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -13,8 +13,7 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 }
 
 function addElementToEndOfArray(array, element){
-  [array,...element];
-  return array;
+  return [array,...element];
 }
 function destructivelyAddElementToEndOfArray(array,element) {
  array.push(element);
