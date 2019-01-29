08f317549d10051a36818dea66b5f33dd7559cc5
Jerry Velazquez <jvr572@gmail.com>
2019-01-29 20:24:05 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -8,7 +8,8 @@ function addElementToBeginningOfArray(array, element){
 }
   
 function destructivelyAddElementToBeginningOfArray(array, element){
-  return array.unshift(element);
+  array.unshift(element)
+  return array;
 }
 
 function addElementToEndOfArray(array, element){
